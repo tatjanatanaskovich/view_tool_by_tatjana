55b08b09e151f9d97d4d8550099cc8828b227174
Tanja <tatjanatanaskovich@gmail.com>
2018-08-15 12:48:07 +0200

    View Tool initial commit

---
+++ lib/view_tool_by_tatjana.rb
@@ -0,0 +1,6 @@
+require "view_tool_by_tatjana/version"
+require "view_tool_by_tatjana/renderer"
+
+module ViewToolByTatjana
+  # Your code goes here...
+end
---
+++ lib/view_tool_by_tatjana/version.rb
@@ -0,0 +1,3 @@
+module ViewToolByTatjana
+  VERSION = "0.1.0"
+end
